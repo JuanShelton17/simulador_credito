b81c09e39ddb56e5ba09faf954b5d7cb00f9122b
juan <juan.shelton17@gmail.com>
2025-01-07 18:06:37 -0300

    primeiro commit

---
+++ src/test/java/com/simuladorcredito/SimuladorCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simuladorcredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
